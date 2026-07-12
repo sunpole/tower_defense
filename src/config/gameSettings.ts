ff8180c55556dbc72e836fb839a93fd77f84771f
ANTON MAHOMEDOV <akitsatnafiya@gmail.com>
2026-07-12 04:57:55 +0300

    Увеличить число волн до десяти

--- src/config/gameSettings.ts
+++ src/config/gameSettings.ts
@@ -7,7 +7,7 @@ export const BOARD_COLUMNS = 12;
 export const BOARD_ROWS = 8;
 export const STARTING_ENERGY = 180;
 export const STARTING_BASE_HEALTH = 10;
-export const TOTAL_WAVES = 5;
+export const TOTAL_WAVES = 10;
 export const SPAWN_INTERVAL_MS = 650;
 export const TICK_MS = 100;
 
